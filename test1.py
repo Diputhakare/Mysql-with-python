64cffc48c3043ddf1b784fcaeb5cf45757db92a7
diputhakare <thakaredipu247@gmail.com>
2022-09-15 20:44:06 +0530

    #MySQL with Python
    Signed-off-by: diputhakare <thakaredipu247@gmail.com>

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ cursor.execute("create database dipmala")
 s = "create table dipmala.details(employe_id int(10)  , emp_name varchar(80) , emp_mailid varchar(20),emp_salary int(6) , emp_attendence int(3))"
 q1 = cursor.execute(s)
 
-q2 = cursor.execute("select * from MSPY.demo1")
+q2 = cursor.execute("select * from dipmala.details")
 print(q2)
 
 
